16506cbf69d4146973ffd6e621de92e3dc1d4daf
Brent <brent.ertz@gmail.com>
2012-11-22 09:27:16 -0700

    Remove query from require.js as it is being loaded separately via CDN

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,12 @@ require.config({
 
   paths: {
     underscore: 'vendor/underscore-min',
-    jquery: 'vendor/jquery.min'
+    jquery: 'vendor/jquery.min' // Loaded separately via CDN
   }
 });
 
-requirejs(['jquery', 'underscore', 'app/app'],
-function($, _, App){
+requirejs(['underscore', 'app/app'],
+function(_, App){
   $(function(){
     var app = new App().init();
     window.app = app;
